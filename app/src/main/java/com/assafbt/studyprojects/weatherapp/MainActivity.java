453f929ed5a270b1a77fe7f7e7ac49b5c178b025
assafbt <assafbt@gmail.com>
2016-01-05 18:11:23 +0200

    Initial commit

---
+++ app/src/main/java/com/assafbt/studyprojects/weatherapp/MainActivity.java
@@ -0,0 +1,21 @@
+/*
+ * *
+ *  * Created by assafbt on <Date>
+ *
+ */
+
+package com.assafbt.studyprojects.weatherapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //init1
+}
