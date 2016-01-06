32478b501b39db70ec562af571099b76ef4cdc32
assafbt <assafbt@gmail.com>
2016-01-06 06:04:14 +0200

    PermissionManager.java

--- app/src/main/java/com/assafbt/studyprojects/weatherapp/MainActivity.java
+++ app/src/main/java/com/assafbt/studyprojects/weatherapp/MainActivity.java
@@ -1,16 +1,20 @@
 /*
  * *
- *  * Created by assafbt on <Date>
+ *  * Created by assafbt on 05/01/2016
  *
  */
 
 package com.assafbt.studyprojects.weatherapp;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,6 +22,13 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     Spinner spinner;
+    ListView arrayView;
+    ArrayAdapter arrayAdapter;
+    ArrayAdapter adapter;
+    private PermissionManager permissionManager;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private Location thisLocation;
 
 
     @Override
@@ -25,17 +36,26 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        arrayView = (ListView) findViewById(R.id.ArraylistViewrRight);
+
         spinner = (Spinner) findViewById(R.id.spinner);
-        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.myLocations, android.R.layout.simple_spinner_item);
-        spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
+
+        // begin Array Adapter
+        adapter = ArrayAdapter.createFromResource(this, R.array.myLocations, android.R.layout.simple_spinner_item);
+        spinner.setAdapter(adapter);
+
+        // end Array Adapter
+
 
     }
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
         TextView myText = (TextView) view;
-        Toast.makeText(this,"location: "+ myText.getText(), Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,"Weather in "+ myText.getText(), Toast.LENGTH_SHORT).show();
+
+
     }
 
     @Override
